5d46a2830f79b5dd37c1e4e4bfd0403467fd0dd9
DANGAL159 <danielgalvez777@gmail.com>
2024-07-11 15:22:27 -0600

    Metodo copyArea para carga de imagenes

--- ventana-swing/src/graficos/LaminaConImagen.java
+++ ventana-swing/src/graficos/LaminaConImagen.java
@@ -21,6 +21,8 @@ public class LaminaConImagen extends JPanel{
         
         g.drawImage(imagen, 5, 5, null); // imagen, posX, posY, observador(quien revisa la imagen) = null
 
+        //El metodo g. copyArea() el cual nos permite crear una copia de nuestra imagen en caso que querramos hacer un mosaico o alguna forma de mostrar imagenes en varios sitios, usa de parametros (posX,posY,ancho,alto, posX a copiar, posY a copiar), utilizarias ciclos 
+        //En caso de no saber el tamaño de tu imagen utilizar .getHeight y gettWidth, y piden el objeto que espera que la imagen sea cargada y es la lamina, asi que usaras  this
 
 
     }
